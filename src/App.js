613986b8249646ef573830a093476602707321c0
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-11-12 01:32:42 -0600

    Working demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { useEffect, useState } from 'react';
 import {Form} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
 import {Modal} from 'react-bootstrap';
@@ -21,6 +20,7 @@ class App extends React.Component{
     }
   }
 
+
   componentDidMount(){
     var pokemonRand = Math.floor(Math.random() * 898) + 1;
     const url = apiData.apiUrl + apiData.endpoint + pokemonRand;
@@ -38,7 +38,41 @@ class App extends React.Component{
         })
   }
 
+  myInput = React.createRef();
+  takeNumber = (e) => {
+    e.preventDefault();
+    console.log(this.myInput.current.value);
+    const url = apiData.apiUrl + apiData.endpoint + this.myInput.current.value;
+    fetch(url)
+        .then((data) => data.json())
+        .then((pokemon) => {
+            this.setState({
+              pokeName: pokemon.name,
+              pokeImg: pokemon.sprites.front_default
+            })
+            console.log(pokemon.name, pokemon.sprites.front_default)
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+  }
   render(){
+
+    /*function findPokemon(id){
+      const url = apiData.apiUrl + apiData.endpoint + id;
+      fetch(url)
+          .then((data) => data.json())
+          .then((pokemon) => {
+              this.setState({
+                pokeName: pokemon.name,
+                pokeImg: pokemon.sprites.front_default
+              })
+              console.log(pokemon.name, pokemon.sprites.front_default)
+          })
+          .catch((err) => {
+              console.error(err);
+          })
+    }*/
     return(
       <div className='container'>
         <Helmet>
@@ -50,6 +84,15 @@ class App extends React.Component{
             <Modal.Body>
               <h1 className='text-center'>Poke Finder!</h1>
               <h1 className='fs-5 badge bg-primary text-wrap'>Ingresa un numero de Pokemon y descubre sus datos</h1>
+              <Form onSubmit={this.takeNumber}>
+                <Form.Group className="mb-3 p-1" controlId="formBasicPassword">
+                  <Form.Label>¡Ingresa el Pokemon que deseas conocer!</Form.Label>
+                  <Form.Control type="text" ref={this.myInput} placeholder="" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
             </Modal.Body>
           </div>
         </Modal.Dialog>
